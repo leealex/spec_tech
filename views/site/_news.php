3b0c12e7bb54bc41089f57380b7a62b2a09852f4
leealex <alex@plumy.ru>
2016-07-18 19:57:32 +0500

    News

--- views/site/_news.php
+++ views/site/_news.php
@@ -4,7 +4,7 @@
 ?>
 <li class="news-item">
     <div class="news-title"><?= $model->title ?></div>
-    <div class="news-date"><?= date('d.m.Y', $model->created_at) ?></div>
+    <div class="news-date">Дата: <?= date('d.m.Y', $model->created_at) ?></div>
     <div class="news-body"><?= $model->body ?></div>
 </li>
 <hr>
