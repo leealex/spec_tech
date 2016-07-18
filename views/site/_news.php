b1c14450e1c2dcae993d2c2c2e5243d1280ee455
leealex <alex@plumy.ru>
2016-07-18 18:29:06 +0500

    News

--- views/site/_news.php
+++ views/site/_news.php
@@ -3,9 +3,8 @@
 
 ?>
 <li class="news-item">
-    <div class="time-badge"><?= date('d.m.Y', $model->created_at) ?></div>
-    <h4><?= $model->title ?></h4>
-    <div class="news-body">
-        <?= $model->body ?>
-    </div>
+    <div class="news-title"><?= $model->title ?></div>
+    <div class="news-date"><?= date('d.m.Y', $model->created_at) ?></div>
+    <div class="news-body"><?= $model->body ?></div>
 </li>
+<hr>
