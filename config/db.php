0a1c3d7d3c8dba454aeedcf8915340a841fd9389
Alexey Lee <alex@plumy.ru>
2019-08-02 18:17:44 +0500

    Delete db.php

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=spec',
-    'username' => 'spec',
-    'password' => 'spec',
-    'charset' => 'utf8',
-];
