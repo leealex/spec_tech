e3bf76bef4c522448461e929ad104ac012672a8c
Alexey Lee <alex@plumy.ru>
2019-10-02 10:54:34 +0600

    Map

--- views/site/contacts.php
+++ views/site/contacts.php
@@ -1,29 +1,26 @@
 <?php
+/**
+ * @var $this yii\web\View
+ * @var $model \app\modules\admin\models\Feedback
+ */
 
-/* @var $this yii\web\View */
-/* @var $model \app\modules\admin\models\Feedback */
-
-use app\modules\admin\models\Settings;
 use app\modules\admin\widgets\Text;
-use app\widgets\Gis;
 
 ?>
 <div class="site-contacts">
-    <section class="">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1">
-                    <div class="icon contacts wow bounceInLeft"></div>
-                    <h2 class="wow bounceInLeft">Контакты</h2>
-                    <?= Text::widget([
-                        'key' => 'contacts',
-                        'htmlOptions' => ['class' => 'wow bounceInRight']
-                    ]) ?>
-                </div>
-            </div>
+  <section class="">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+          <div class="icon contacts wow bounceInLeft"></div>
+          <h2 class="wow bounceInLeft">Контакты</h2>
+            <?= Text::widget([
+                'key' => 'contacts',
+                'htmlOptions' => ['class' => 'wow bounceInRight']
+            ]) ?>
         </div>
-    </section>
-
-    <iframe src="https://yandex.ru/map-widget/v1/-/CBa8bBWW8D" width="100%" height="500" frameborder="0"></iframe>
-    
+      </div>
+    </div>
+  </section>
+  <iframe src="https://yandex.ru/map-widget/v1/-/CGgdVSk~" width="100%" height="500" frameborder="0"></iframe>
 </div>
