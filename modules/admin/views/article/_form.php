924c2d0531699db0ac734c440f60761407476efe
Alexey Lee <alex@plumy.ru>
2020-05-12 13:51:41 +0600

    Redesign

--- modules/admin/views/article/_form.php
+++ modules/admin/views/article/_form.php
@@ -1,8 +1,8 @@
 <?php
 
 use app\modules\admin\widgets\ButtonGroup;
-use app\modules\admin\widgets\ImageBrowser;
 use mihaildev\ckeditor\CKEditor;
+use mihaildev\elfinder\InputFile;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use mihaildev\elfinder\ElFinder;
@@ -27,7 +27,8 @@ use yii\widgets\MaskedInput;
       <div class="row">
         <div class="col-md-2"><?= $form->field($model, 'createdAt')
                 ->widget(MaskedInput::class, ['mask' => '99.99.9999', 'options' => ['placeholder' => 'ДД.ММ.ГГГГ']]) ?></div>
-        <div class="col-md-2"><?= $form->field($model, 'category_id')->dropDownList($categories, ['prompt' => '']) ?></div>
+        <div
+            class="col-md-2"><?= $form->field($model, 'category_id')->dropDownList($categories, ['prompt' => '']) ?></div>
         <div class="col-md-4"><?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?></div>
         <div class="col-md-4"><?= $form->field($model, 'slug')->textInput(['maxlength' => true]) ?></div>
       </div>
@@ -38,6 +39,17 @@ use yii\widgets\MaskedInput;
                     ['preset' => 'full', 'extraAllowedContent' => ['p[*]', 'a[*]', 'video[*]', 'source[*]', '*(*){*}']]),
             ]) ?>
         </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+            <?= $model->thumbnail_path ? Html::img($model->thumbnail_path) : Html::img('/img/no-image.png') ?>
+            <?= $form->field($model, 'thumbnail_path')->widget(InputFile::class, [
+                'controller' => 'admin/elfinder',
+                'filter' => 'image',
+                'template' => '<div class="input-group">{input}<span class="input-group-btn">{button}</span></div>',
+                'buttonOptions' => ['class' => 'btn btn-default'],
+                'multiple' => false
+            ]); ?></div>
       </div>
       <div class="row">
         <div class="col-md-12">
